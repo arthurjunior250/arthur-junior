8234d867a0dea76fe65040ca605150dc348749e2
arthurjunior250 <arthurjunior88741@gmail.com>
2020-08-15 12:36:53 +0200

    full web 11

---
+++ js/java.js
@@ -0,0 +1,20 @@
+function message(){
+alert("welcome to Mr. Arthur Junior page")
+}
+function myfunction(){
+var x=document.getElementByID("touch");
+x.value=x.value.tolowercase();
+}
+function myfocus(x){
+x.style.background="while"
+}
+function message1(){
+alert("do you want to leave this page.")
+}
+function zoom(){
+	// x=document.getElementByTagName("img");
+	// x.style.width=100%;
+	// x.style.height=100%;
+}
+
+
