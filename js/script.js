3b197f2055e157e9fec7d364777cf2c29158b404
arthurjunior250 <arthurjunior88741@gmail.com>
2020-08-26 07:02:54 +0200

    git

--- js/script.js
+++ js/script.js
@@ -66,19 +66,6 @@ var switchMenuToActive = function () {
   }
 };
 
-// On page load (before images or CSS)
-document.addEventListener("DOMContentLoaded", function (event) {
-
-// On first load, show home view
-showLoading("#main-content");
-$ajaxUtils.sendGetRequest(
-  homeHtml,
-  function (responseText) {
-    document.querySelector("#main-content")
-      .innerHTML = responseText;
-  },
-  false);
-});
 
 // Load the menu categories view
 dc.loadMenuCategories = function () {
