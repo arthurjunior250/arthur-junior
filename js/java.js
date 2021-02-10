985887b90ac45aa589d45f03174385c5229814cc
arthurjunior250 <arthurjunior88741@gmail.com>
2021-02-10 14:03:09 +0200

    2021

--- js/java.js
+++ js/java.js
@@ -1,6 +1,6 @@
-function message(){
-alert("welcome to Mr. Arthur Junior page. to day is "+ Date());
-}
+// function message(){
+// alert("welcome to Mr. Arthur Junior page. to day is "+ Date());
+// }
 function myfunction(){
 var x=document.getElementByID("touch");
 x.value=x.value.tolowercase();
@@ -11,6 +11,27 @@ x.style.background="while"
 function message1(){
 alert("Do You Want To Leave This Page.")
 }
+
+function loader(){
+  // // show loader
+  document.getElementById('loading').style.display = 'show';
+  
+  // // hide body
+  document.getElementById('start').style.display = 'none';
+
+  setTimeout(showBody, 3000);
+
+};
+
+function showBody(){
+   // // show body
+   document.getElementById('start').style.display = 'block';
+
+    // // hide loader
+  document.getElementById('loading').style.display = 'none';
+
+}
+
 
 
 
